261ae5d6a70c04d174adb32597cb64db256f027b
Alex <alexbonniard@gmail.com>
2024-06-26 14:16:03 +0200

    Ajout formulaire + début modale

--- Backend/login.js
+++ Backend/login.js
@@ -7,14 +7,39 @@ async function envoiFormulaire() {
             password: document.querySelector("#password").value
         }
         const chargeUtile = JSON.stringify(identifiant)
-        fetch("http://localhost:5678/api/users/login", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: chargeUtile
+
+        try {
+            const reponse = await fetch("http://localhost:5678/api/users/login", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: chargeUtile
+            })
+
+            if (!reponse.ok) {
+                throw new Error("Erreur HTTP : " + reponse.status);
+            }
+
+            const data = await reponse.json();
+            console.log("Réponse de l'API :", data);
+
+            if (data.token && data.userId) {
+            
+                localStorage.setItem("token", data.token);
+                localStorage.setItem("userId", data.userId);
+
+                window.location.href = "index.html"
+
+            } else {
+                // Si les données ne sont pas correctes, affichez un message d'erreur
+                throw new Error("Données de connexion incorrectes");
+            }
+
+        } catch (error) {
+            console.log(error);
+            const messageErreur = document.querySelector("#messageErreur")
+            messageErreur.innerText = "Vos identifiants sont incorrects."
+        }
         })
-        const haha = await response.json()
-        console.log(haha)
-    })
 }
 
 envoiFormulaire()
