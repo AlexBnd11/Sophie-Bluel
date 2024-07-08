9c5aceaa3948d1c518baaaa6e4dca85cb3cd8569
AlexBnd11 <alexbonniard@gmail.com>
2024-07-08 08:57:04 +0200

    Modale Opérationnelle

--- FrontEnd/assets/scripts/script.js
+++ FrontEnd/assets/scripts/script.js
@@ -1,12 +1,17 @@
+///////////////////////////////////////////////////////////////////////////////
+////////////////////////////    VARIABLES    //////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
 // On recupère les projets et les catégories via l'API
-const swagger = await fetch("http://localhost:5678/api/works");
-const projets = await swagger.json();
+const swagger = await fetch("http://localhost:5678/api/works")
+const projets = await swagger.json()
 const swaggerCategories = await fetch("http://localhost:5678/api/categories")
-const categories = await swaggerCategories.json();
+const categories = await swaggerCategories.json()
 
 // Déclaration des variables
-const gallery = document.querySelector(".gallery");
-const filtres = document.querySelector(".filtres");
+const headerEdition = document.querySelector(".header-edition")
+const gallery = document.querySelector(".gallery")
+const filtres = document.querySelector(".filtres")
 const setCategories = new Set()
 const token = localStorage.getItem("token")
 const loginLogout = document.querySelector(".login-logout")
@@ -18,6 +23,15 @@ const photosModale = document.querySelector(".modifier-photos")
 const sectionAjouterPhotos = document.querySelector(".section-ajouter-photos")
 const divBtnAjouterModale = document.querySelector(".btn-ajouter-modale")
 const btnAjouterModale = document.querySelector(".btn-ajouter-modale button")
+const uploadButton = document.querySelector(".upload button")
+const btnPostWork = document.getElementById("formulaire-ajout")
+const ajouterPhoto = document.getElementById("ajouter-photo")
+const submitBtn = document.getElementById("post-work")
+const inputs = document.querySelectorAll(".input")
+
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////////    PROJETS    ///////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
 genererProjetsEtCategories(projets, categories);
 
@@ -62,11 +76,15 @@ categories.forEach(category => {setCategories.add(category)})
   };
 };
 
-// On gère la couleur des boutons filtres au click via une fontion
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////////    FILTRES    ///////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+// Couleur des boutons filtres au click via une fonction
 function couleurBtnFiltrage(button) {
-        const buttons = document.querySelectorAll(".filtres button, .tous");
-        buttons.forEach(btn => btn.classList.remove("selected"));
-        button.classList.add("selected");
+        const buttons = document.querySelectorAll(".filtres button, .tous")
+        buttons.forEach(btn => btn.classList.remove("selected"))
+        button.classList.add("selected")
       }
 
 
@@ -83,11 +101,14 @@ function filtrage (categorie) {
             <figcaption>${objetsFiltres[i].title}</figcaption>
           </figure>
           `
-          };
-        };
+          }
+        }
 
-// Ajout du bouton modifier si un token est stocké localement
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////    BOUTON MODIFIER    ///////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
+// On fait appraître le bouton "Modifier" si on est connecté
 if (token) {
   loginLogout.innerHTML = `<a href="index.html">logout</a>`
   loginLogout.addEventListener("click", (event) => {
@@ -95,15 +116,19 @@ if (token) {
     localStorage.removeItem("token")
     window.location.href = "index.html"
   })
-  const modifier = document.querySelector(".modifier")
-  modifier.innerHTML = `
+  boutonModifier.innerHTML = `
     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M13.5229 1.68576L13.8939 2.05679C14.1821 2.34503 14.1821 2.81113 13.8939 3.0963L13.0016 3.99169L11.5879 2.57808L12.4803 1.68576C12.7685 1.39751 13.2346 1.39751 13.5198 1.68576H13.5229ZM6.43332 7.73578L10.5484 3.61759L11.9621 5.03121L7.84387 9.14633C7.75494 9.23525 7.64455 9.29964 7.52496 9.33337L5.73111 9.84546L6.2432 8.05162C6.27693 7.93203 6.34133 7.82164 6.43025 7.73271L6.43332 7.73578ZM11.4408 0.646245L5.39074 6.6932C5.12397 6.95998 4.93078 7.28808 4.82959 7.64685L3.9526 10.7133C3.879 10.9708 3.94953 11.2468 4.13965 11.4369C4.32977 11.627 4.60574 11.6976 4.86332 11.624L7.92973 10.747C8.29156 10.6427 8.61967 10.4495 8.88338 10.1858L14.9334 4.13888C15.7951 3.27722 15.7951 1.87894 14.9334 1.01728L14.5624 0.646245C13.7007 -0.215415 12.3024 -0.215415 11.4408 0.646245ZM2.69844 1.84214C1.20816 1.84214 0 3.05031 0 4.54058V12.8812C0 14.3715 1.20816 15.5796 2.69844 15.5796H11.0391C12.5293 15.5796 13.7375 14.3715 13.7375 12.8812V9.44683C13.7375 9.039 13.4094 8.71089 13.0016 8.71089C12.5937 8.71089 12.2656 9.039 12.2656 9.44683V12.8812C12.2656 13.5589 11.7167 14.1078 11.0391 14.1078H2.69844C2.02076 14.1078 1.47188 13.5589 1.47188 12.8812V4.54058C1.47188 3.86291 2.02076 3.31402 2.69844 3.31402H6.13281C6.54065 3.31402 6.86875 2.98591 6.86875 2.57808C6.86875 2.17025 6.54065 1.84214 6.13281 1.84214H2.69844Z" fill="black"/>
     </svg>
     modifier
     </a>
     `
+  headerEdition.style.display = null
 }
+
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////    MODALE    ///////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
 function afficherModale(e) {
   e.preventDefault()
@@ -122,13 +147,15 @@ function fermerModale(e) {
 boutonModifier.addEventListener("click", afficherModale)
 croixModale.addEventListener("click", fermerModale)
 
+// On ferme la modale si on clique à côté
 modale.addEventListener("click", (e) => {
   const modaleWrapper = document.querySelector(".modale-wrapper");
   if (!(modaleWrapper.contains(e.target))) {
-    fermerModale();
+    fermerModale()
   }
 });
 
+// Génération des photos et l'icône "supprimer" dans la modale
 function genererPhotosModale() {
   photosModale.innerHTML = ``
   for (let i = 0; i < projets.length; i++) {
@@ -150,6 +177,10 @@ function genererPhotosModale() {
   supprimerPhotosModale()
 }
 
+///////////////////////////////////////////////////////////////////////////////
+///////////////////   SUPPRESSION/AJOUT PHOTOS MODALE   ///////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
 function supprimerPhotosModale() {
   const trashIcons = document.querySelectorAll(".trash")
   trashIcons.forEach(icon => {
@@ -157,24 +188,108 @@ function supprimerPhotosModale() {
         const iconId = this.id
         const urlApi = "http://localhost:5678/api/works/" + iconId
         console.log(token)
-          const response = await fetch(urlApi, {
+              fetch(urlApi, {
               method: "DELETE",
               headers: {
                 "Content-Type": "application/json",
                 "Authorization": `Bearer ${token}`
               },
           })
-    )
+  })
 })
 }
 
-
-function ajouterUnePhoto() {
+// On change le contenu de la modale au clic du bouton
+btnAjouterModale.addEventListener("click", (e) => {
   titreModale.innerHTML = `Ajout Photo`
   sectionAjouterPhotos.style.display = null
   photosModale.innerHTML = ``
   photosModale.classList.add("hidden")
   divBtnAjouterModale.classList.add("hidden")
+  categoriesFormulaire()
+})
+
+// Impossibilité de submit le formulaire quand le formulaire n'est pas rempli
+inputs.forEach(input => {
+  input.addEventListener("input", checkFormulaire)
+})
+
+function checkFormulaire() {
+  let inputCheck = true
+  inputs.forEach(input => {
+    if (!input.value) {
+      inputCheck = false
+    }
+  })
+  if (inputCheck) {
+  submitBtn.disabled = false
+  submitBtn.classList.remove("disabled")
+  } else {
+  submitBtn.disabled = true
+  submitBtn.classList.add("disabled")
+  }
 }
 
-btnAjouterModale.addEventListener("click", (e) => {ajouterUnePhoto()})
+// On génère dynamiquement les catégories dans le formulaire
+function categoriesFormulaire() {
+  const selectCategorie = document.querySelector("#ajout-categorie-projet")
+  selectCategorie.innerHTML = ``
+  for (let i = 0; i < categories.length; i++) {
+    const nomCategorie = categories[i].name
+    const select = document.createElement("option")
+    select.id = i + 1
+    select.textContent = nomCategorie
+    selectCategorie.appendChild(select)
+  }
+}
+
+// On assigne le déclenchement de l'input[type="file"] au bouton visible
+uploadButton.addEventListener("click", function(e) {
+  e.preventDefault()
+  document.getElementById("ajouter-photo").click()
+})
+
+// Prévisualisation de l'image que l'on va ajouter
+ajouterPhoto.addEventListener("change", () => {
+  const preview = document.getElementById("preview")
+  const file = ajouterPhoto.files[0]
+
+  if (file) {
+    const elements = document.querySelectorAll('.bloc-ajouter-photos > *');
+    elements.forEach(element => {
+      if (element.tagName.toLowerCase() !== 'img') {
+          element.style.display = 'none'
+      }
+    })
+    const reader = new FileReader()
+        reader.onload = e => {
+            preview.src = e.target.result
+        }
+        reader.readAsDataURL(file)
+  }
+})
+
+// Au submit du formulaire, on appelle l'API pour ajouter un projet
+// dynamiquement à la galerie via une fonction anonyme
+btnPostWork.addEventListener("submit", e => {
+  e.preventDefault()
+  const data = new FormData() 
+  data.append('image', document.getElementById('ajouter-photo').files[0]);
+  data.append('title', document.getElementById('titre-projet').value);
+  data.append('category', document.getElementById('ajout-categorie-projet').selectedOptions[0].id);
+
+  fetch("http://localhost:5678/api/works", {
+    method: 'POST',
+    headers: {
+      "Authorization": `Bearer ${token}`
+  },
+    body: data
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+})
